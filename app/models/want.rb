1247bf8bb0394e07a33385467e0720910661992e
Grant Speelman <grantspeelman@gmail.com>
2012-01-18 15:49:26 +0200

    added tests for wants

--- app/models/want.rb
+++ app/models/want.rb
@@ -9,4 +9,8 @@ class Want
   validates_uniqueness_of :card_name, :scope => :user_id
   validates_numericality_of :amount, :greater_than => 0, :only_integer => true
   validates_numericality_of :priority, :greater_than => 0, :less_than => 4, :only_integer => true
+
+  scope :by_card_name, ->(name) { where(:card_name => name) }
+
 end
+
