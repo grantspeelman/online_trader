a564880dffbb147216d8d2f475c1e4362c36437e
Grant Speelman <grantspeelman@gmail.com>
2012-01-19 13:50:14 +0200

    helper function for main menu items

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,10 @@ module ApplicationHelper
     content_for(:side_nav){content_tag("li",link_to(body, url, html_options))}
   end
 
+  def main_nav_link(body, url, html_options = {})
+    li_class = nil
+    li_class = "active" if current_page?(url)
+    content_tag("li",link_to(body, url, html_options),:class => li_class)
+  end
+
 end
