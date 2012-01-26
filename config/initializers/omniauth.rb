98bedad1a52733d1385a6df538932a25d25071af
Grant Speelman <grantspeelman@gmail.com>
2012-01-26 17:03:52 +0200

    added google auth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,5 +3,6 @@ require 'openid/store/filesystem'
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 'NC5rkqjoiWmQqMqg9hJZA', '50crDzCTln7h07FhrD4dfsD0PNqxsOKtBz7HE7OXM'
   provider :facebook, '326713784029745', '31c7237b4280f806a1ecb5ebd4482447', :grant_type => 'client_credentials'
+  provider :google, '363313196425.apps.googleusercontent.com', 'WkaDfmQjwdFrYt4l3GO2YgcE'
   # use OmniAuth::Strategies::OpenID, OpenID::Store::Filesystem.new(Rails.root.join('tmp'))
 end
--- spec/support/oauth.rb
+++ spec/support/oauth.rb
@@ -1,6 +1,7 @@
 OmniAuth.config.test_mode = true
 OmniAuth.config.add_mock(:twitter, { :uid => '11111', :info => {:name => "Billy Bob"}})
 OmniAuth.config.add_mock(:facebook, { :uid => '22222', :info => {:name => "Grant Speelman"}})
+OmniAuth.config.add_mock(:google, { :uid => '33333', :info => {:name => "James Bond"}})
 
 module OauthHelper
 
