e48428070b4b0f989a49aa1b00d63497cc5078ba
Grant Petersen-Speelman <grantp@jobready.com.au>
2018-07-23 11:21:58 +1000

    set cypress folder

--- test.sh
+++ test.sh
@@ -9,7 +9,7 @@ bundle exec ./bin/rails server -p 5002 -e test &
 sleep 2 # give rails a chance to start up correctly
 
 echo '-- cypress run'
-node_modules/.bin/cypress run
+node_modules/.bin/cypress run -P spec
 
 echo '-- stop rails server'
 kill -9 `cat tmp/pids/server.pid`
