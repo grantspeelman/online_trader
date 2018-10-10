c14a3da96b1a01f25cdd48fae09a8cfa2c8a696e
Grant Petersen-Speelman <grantp@jobready.com.au>
2018-10-10 23:52:00 +1100

    amazon cognito

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,4 +2,12 @@
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer
+
+  use OmniAuth::Strategies::CognitoIdP,
+      ENV['CLIENT_ID'],
+      ENV['CLIENT_SECRET'],
+      client_options: {
+        site: ENV['COGNITO_USER_POOL_SITE']
+      },
+      scope: 'nickname phone email openid aws.cognito.signin.user.admin profile'
 end
